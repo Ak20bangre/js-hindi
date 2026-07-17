9115fafa29ba9eb5aea1a72661ffb665111bb057
Aditya Kumar bangre <166615154+Ak20bangre@users.noreply.github.com>
2026-07-17 06:47:53 +0000

    conversion of datatypes

---
+++ basics/conversion.js
@@ -0,0 +1,18 @@
+let score="33abc"
+let points= null
+
+console.log(typeof(score))
+console.log(typeof(points))
+
+let valueInNumber=Number(score)
+let pointsInNumber=Number(points)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+console.log(typeof pointsInNumber)
+console.log(pointsInNumber)
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1, false => 0
