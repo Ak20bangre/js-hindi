53c8667cb82bdedea3643a96ede898697e1022a3
Aditya Kumar bangre <166615154+Ak20bangre@users.noreply.github.com>
2026-07-17 06:30:58 +0000

    variables,dataypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,6 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3)  //this wont run as we are using node JS and not Browser
+
+let age=18
+console.log(typeof age);
---
+++ basics/variable.js
@@ -0,0 +1,9 @@
+const accountId=14453
+let accountEmail="ak20bangre@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+// perfer not to use var due to issue in block scope and functional scope
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
