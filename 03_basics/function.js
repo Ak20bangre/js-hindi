dbb493be707c354448e14f91579a03f5f802ce8f
Aditya Kumar bangre <166615154+Ak20bangre@users.noreply.github.com>
2026-07-18 10:44:35 +0000

    function

---
+++ 03_basics/function.js
@@ -0,0 +1,20 @@
+function calculateCartPrice(...num1){   //...num1 is used to pass any number of arguments to a function
+    return num1
+}
+
+console.log(calculateCartPrice(200,400,500))
+
+
+/*
+const user={
+    username:"aditya",
+    price:199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+handleObject(user)
+*/
+
